cae0e1b7d422787553e95e7cba02d1fbf91d9232
julzliang <jl12397@nyu.edu>
2025-12-18 21:03:48 -0500

    framework installed

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-green-500 p-2 sm:p-10 md:p-20 lg:p-40">
+      <h1 className="text-white text-3xl">Resize the window</h1>
+    </div>
+  );
+}
